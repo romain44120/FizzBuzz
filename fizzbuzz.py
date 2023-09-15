789bdea0e1c8c6f9cc623addad5adc8ef0009bbd
romain44120 <80761067+romain44120@users.noreply.github.com>
2023-09-15 12:22:28 +0200

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -8,7 +8,15 @@ def fizzBuzz(n):
     else:
         return str(n)
 
-# Demande à l'utilisateur d'entrer un nombre
-num = int(input("Entrez un nombre entier : "))
-result = fizzBuzz(num)
-print("Résultat de FizzBuzz pour", num, ":", result)
+# Tests unitaires
+def test_fizzBuzz():
+    assert fizzBuzz(1) == "1"
+    assert fizzBuzz(3) == "Fizz"
+    assert fizzBuzz(5) == "Buzz"
+    assert fizzBuzz(15) == "FizzBuzz"
+    assert fizzBuzz(7) == "7"
+    assert fizzBuzz(30) == "FizzBuzz"
+
+if __name__ == "__main__":
+    test_fizzBuzz()
+    print("Tous les tests sont réussis !")
